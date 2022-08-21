91dba280612c32cd36311ff63a770520ae8c2ba1
edward-tx <>
2022-08-21 15:58:34 -0500

    8kyu - even or odd

--- codewars.js
+++ codewars.js
@@ -5,7 +5,23 @@
 //   }
 //
 // change to: return a * b;
-function multiply(a, b) {
-    return a * b;
-}
-console.log(multiply(5,6));
+// function multiply(a, b) {
+//     return a * b;
+// }
+// console.log(multiply(5,6));
+// best solution
+// multiple = (a, b) => a * b;
+//
+// 8kyu - Even or Odd
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// function even_or_odd(number) {
+//
+//}
+even_or_odd = (num) => num %2 === 0 ? "Even" : "Odd";
+console.log(even_or_odd(5));
+console.log(even_or_odd(4));
+console.log(even_or_odd(8));
+//best solution
+// function even_or_odd(number) {
+//     return number % 2 ? "Odd" : "Even"
+//   }
