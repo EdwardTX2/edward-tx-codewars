63e45c43b37a4bbcf2a8a9d8b92508dc1671a17d
EdwardTX2 <78395896+EdwardTX2@users.noreply.github.com>
2022-08-21 15:30:34 -0500

    8kyu - multiply

---
+++ codewars.js
@@ -0,0 +1,11 @@
+//8kyu - Multiply
+//This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//     a * b
+//   }
+//
+// change to: return a * b;
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(5,6));
