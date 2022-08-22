ab1f7dbebe3e8ff1710227f87bb13da7c68adeae
edward-tx <>
2022-08-22 14:51:02 -0500

    8kyu - Opposites Attract

--- 20220822.js
+++ 20220822.js
@@ -200,12 +200,16 @@
 // 8kyu Beginner Series #1 School Paperwork
 // Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
 // Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
-function paperwork(n, m) {
-  if (n < 0 || m < 0) { return 0; }
-  return n*m;
-}
-console.log(paperwork(5,5), 25, 'Failed at paperwork(5, 5)');
-console.log(paperwork(5,-5), 0, 'Failed at paperwork(5, -5)');
-console.log(paperwork(-5,-5), 0, 'Failed at paperwork(-5, -5)');
-console.log(paperwork(-5,5), 0, 'Failed at paperwork(-5, 5)');
-console.log(paperwork(5,0), 0, 'Failed at paperwork(5, 0)');
+// function paperwork(n, m) {
+//   if (n < 0 || m < 0) { return 0; }
+//   return n*m;
+// }
+// console.log(paperwork(5,5), 25, 'Failed at paperwork(5, 5)');
+// console.log(paperwork(5,-5), 0, 'Failed at paperwork(5, -5)');
+// console.log(paperwork(-5,-5), 0, 'Failed at paperwork(-5, -5)');
+// console.log(paperwork(-5,5), 0, 'Failed at paperwork(-5, 5)');
+// console.log(paperwork(5,0), 0, 'Failed at paperwork(5, 0)');
+//
+// 8kyu Opposites Attract
+// if one num is even, the other is odd, return true
+const lovefunc = (f1, f2) => (f1 + f2) % 2 == 1
