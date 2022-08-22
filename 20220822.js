35bfe0f0173b591a83950b685311f41987dca5a9
edward-tx <>
2022-08-22 14:41:08 -0500

    8kyu - Beginner Series #1 School Paperwork

--- 20220822.js
+++ 20220822.js
@@ -184,15 +184,28 @@
 // function invert(array) {
 //     return ;
 // }
-function invert(array) {
-    let arr = array.map(function(num) {
-        if (num > 0) {
-            return -num;
-        } else {
-            return Math.abs(num);
-        }
-    })
-    return arr;
-}
+// function invert(array) {
+//     let arr = array.map(function(num) {
+//         if (num > 0) {
+//             return -num;
+//         } else {
+//             return Math.abs(num);
+//         }
+//     })
+//     return arr;
+// }
 // better solution:
 // const invert = array => array.map(num => -num);
+//
+// 8kyu Beginner Series #1 School Paperwork
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+function paperwork(n, m) {
+  if (n < 0 || m < 0) { return 0; }
+  return n*m;
+}
+console.log(paperwork(5,5), 25, 'Failed at paperwork(5, 5)');
+console.log(paperwork(5,-5), 0, 'Failed at paperwork(5, -5)');
+console.log(paperwork(-5,-5), 0, 'Failed at paperwork(-5, -5)');
+console.log(paperwork(-5,5), 0, 'Failed at paperwork(-5, 5)');
+console.log(paperwork(5,0), 0, 'Failed at paperwork(5, 0)');
