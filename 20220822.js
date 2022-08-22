a34d04489a5afaa74a1363139b3f34c076f00797
edward-tx <>
2022-08-22 16:32:57 -0500

    8kyu - Is he gonna survive?

--- 20220822.js
+++ 20220822.js
@@ -252,14 +252,21 @@
 //
 // 8kyu Calculate average
 // Write a function which calculates the average of the numbers in a given list.
-function find_average(array) {
-    if (array.length < 1) {
-      return 0
-    } else {
-      return array.reduce((a, b) => a + b, 0) / array.length;
-    }
-}
+// function find_average(array) {
+//     if (array.length < 1) {
+//       return 0
+//     } else {
+//       return array.reduce((a, b) => a + b, 0) / array.length;
+//     }
+// }
 // Another solution:
 // function find_average(arr) {
 //     return arr.length > 0? arr.reduce((a, b) => a + b) / arr.length : 0;
+// }
+//
+// 8kyu Is he gonna survive?
+const hero = (b, d) => b / d >= 2
+// Common solution:
+// function hero(bullets, dragons){
+//     return bullets >= dragons * 2
 // }
