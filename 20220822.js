0edf4bc944804e5dc19405079715fc85a3472c59
edward-tx <>
2022-08-22 08:21:55 -0500

    8kyu - Century from Year

--- 20220822.js
+++ 20220822.js
@@ -3,11 +3,25 @@
 // function litres(time) {
 //     return 0;
 // }
-const litres = (time) => parseInt((time/2))
-console.log(litres(2))
-console.log(litres(1.4))
-console.log(litres(12.3))
+// const litres = (time) => parseInt((time/2))
+// console.log(litres(2))
+// console.log(litres(1.4))
+// console.log(litres(12.3))
 // Most used solution:
 // function litres(time) {
 //     return Math.floor(time * 0.5);
 // }
+//
+// 8kyu - Century from Year
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+// Given the year, return the centry it is in
+// function century(year) {
+//     // Finish this :)
+//     return;
+// }
+const century = (year) => Math.ceil(year *.01)
+console.log(century(1705))
+console.log(century(1900))
+console.log(century(1601))
+// common solution:
+// const century = year => Math.ceil(year/100)
